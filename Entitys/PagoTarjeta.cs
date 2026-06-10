c0f95b568f57b5952507b1e287cf69e11470a5e9
Sofia Rasi <sofiarasi25@gmail.com>
2026-06-10 20:03:21 -0300

    Creo la clase PagoTarjeta

---
+++ Entitys/PagoTarjeta.cs
@@ -0,0 +1,11 @@
+class PagoTarjeta : Pago
+{
+    public PagoTarjeta(string titular, double monto) : base(titular, monto)
+    {
+    }
+
+    public override double CalcularComision()
+    {
+        return 2000;
+    }
+}
