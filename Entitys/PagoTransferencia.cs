8fbd94a2d9c19eaa6e842540f09e2046f7730a08
Sofia Rasi <sofiarasi25@gmail.com>
2026-06-10 20:07:00 -0300

    Creo la clase PagoTransferencia

---
+++ Entitys/PagoTransferencia.cs
@@ -0,0 +1,11 @@
+class PagoTransferencia : Pago
+{
+    public PagoTransferencia(string titular, double monto) : base(titular, monto)
+    {
+    }
+
+    public override double CalcularComision()
+    {
+        return 500;
+    }
+}
